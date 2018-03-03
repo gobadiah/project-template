9b9460aa65757758443eee3cd69c640f8c6fcad6
Michaël Journo <gobadiah@gmail.com>
2018-03-03 15:19:05 +0100

    Web: refactor axios tests

--- web/__tests__/config/axios.js
+++ web/__tests__/config/axios.js
@@ -1,47 +1,25 @@
 import axios from '~/config/axios';
 
-describe('Axios config', () => {
-  it('should have a default value', () => {
-    expect(axios()).toEqual({
-      baseUrl: undefined,
-      withCredentials: true,
-      headers: {},
-    });
-  });
-
-  it('should be possible to add extra headers', () => {
-    const headers = { some: 'headers' };
-    expect(axios({ headers })).toEqual({
-      baseUrl: undefined,
-      withCredentials: true,
-      headers: {
-        some: 'headers',
-      },
-    });
-  });
-
-  it('should have an api params', () => {
-    const api = 'api';
-    expect(axios({ api })).toEqual({
+const testAxios = (message, { api, req, headers }, Cookie) => {
+  it(message, () => {
+    expect(axios({ api, req, headers })).toEqual({
       baseUrl: api,
       withCredentials: true,
-      headers: {},
+      headers: Object.assign({}, headers, Cookie ? { Cookie } : {}),
     });
   });
+};
 
-  it('should copy cookies from request', () => {
-    const req = {
+describe('Axios config', () => {
+  testAxios('should have a default value', {});
+  testAxios('should be possible to add extra headers', { headers: { some: 'headers' } });
+  testAxios('should have an api params', { api: 'api' });
+  testAxios('should copy cookies from request', {
+    req: {
       cookies: {
         some: 'value',
         other: 'cookie',
       },
-    };
-    expect(axios({ req })).toEqual({
-      baseUrl: undefined,
-      withCredentials: true,
-      headers: {
-        Cookie: 'some=value; other=cookie',
-      },
-    });
-  });
+    },
+  }, 'some=value; other=cookie');
 });
--- web/src/redux/json-api/auth.js
+++ web/src/redux/json-api/auth.js
@@ -4,23 +4,18 @@ import { signin as signinAction } from '~/redux/auth';
 
 import config from '~/config';
 
-export const signin = dispatch => data => axios.post(
-  '/auth/signin',
+const post = (dispatch, path, data, headers) => axios.post(
+  path,
   data,
-  config.axios(),
+  config.axios(headers),
 ).then((result) => {
   dispatch(hydrateStore(result.data));
   dispatch(signinAction(result.data.data.id));
 });
 
-export const register = dispatch => data => axios.post(
-  '/auth/register',
-  data,
-  config.axios({
-    'Content-Type': 'application/vnd.api+json',
-    Accept: 'application/vnd.api+json',
-  }),
-).then((result) => {
-  dispatch(hydrateStore(result.data));
-  dispatch(signinAction(result.data.data.id));
+export const signin = dispatch => data => post(dispatch, '/auth/signin', data);
+
+export const register = dispatch => data => post(dispatch, '/auth/register', data, {
+  'Content-Type': 'application/vnd.api+json',
+  Accept: 'application/vnd.api+json',
 });
