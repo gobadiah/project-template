a6e9608f2a99d28da70b263204aafd8551d0c09f
Michaël Journo <gobadiah@gmail.com>
2018-03-29 12:33:46 +0200

    Web: Add gender fields

--- web/src/components/forms/fields/index.js
+++ web/src/components/forms/fields/index.js
@@ -3,3 +3,4 @@ export { default as PasswordField } from './password';
 export { default as FirstNameField } from './first-name';
 export { default as LastNameField } from './last-name';
 export { default as BirthdayField } from './birthday';
+export * from './gender';
