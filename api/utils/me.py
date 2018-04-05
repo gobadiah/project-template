d4d406b9d8300de7aed5fac728a65e32661bc0f7
Michaël Journo <gobadiah@gmail.com>
2018-04-05 13:00:34 +0200

    Api: Add get_user utils function to handle 'me' pk (2)

--- api/utils/me.py
+++ api/utils/me.py
@@ -10,7 +10,7 @@ User = get_user_model()
 def get_user(request, kwargs):
     """Retrieve the correct user, using either the id or request.user if me."""
     if kwargs['user_pk'] == 'me':
-        if not request.user or not request.user.is_authenticated():
+        if not request.user or not request.user.is_authenticated:
             raise NotAuthenticated()
         return request.user
     else:
