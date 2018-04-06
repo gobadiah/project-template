61fd26e43bf40d71b6b92223ab92bff81293c5ac
Michaël Journo <gobadiah@gmail.com>
2018-04-06 12:06:24 +0200

    Web: Check user to be defined in currentUser selector (auth)

--- web/src/auth/selectors.js
+++ web/src/auth/selectors.js
@@ -4,6 +4,8 @@ import denormalizer, { removeCircularReferences } from 'json-api-denormalizer';
 // eslint-disable-next-line import/prefer-default-export
 export const currentUser = (state) => {
   const user = _.get(denormalizer(state.api), `users.${state.auth.get('userId')}`);
-  user.toJSON = removeCircularReferences(user);
+  if (user) {
+    user.toJSON = removeCircularReferences(user);
+  }
   return user;
 };
