d15f5d158f22cf2dd8b3b5aefb5b0ff9b38d8fe0
Nicolas Megab <nicolas.megarbane@gmail.com>
2018-04-27 19:10:36 +0200

    web : add buttons and states to heat map

--- web/src/pages/session/components/heatmap.js
+++ web/src/pages/session/components/heatmap.js
@@ -1,6 +1,6 @@
 import { arrayOf, bool, shape, string } from 'prop-types';
 import {defaultPropTypes } from '~/components';
-
+import { css } from 'react-emotion';
 import React from 'react';
 
 /*
@@ -9,10 +9,69 @@ const HeatMap = ({ session, stats }, { t }) => (
 );*/
 
 import { PureComponent } from '~/components/base';
-
-import { HeatMapContainer as Container } from './styles';
+// @ michael how to isntall switch button ?
+import { RoundButton } from '~/components';
+import { HeatMapContainer as Container ,
+          HeatMapOptionContainer as ButtonContainer} from './styles';
 
 class HeatMap extends PureComponent {
+
+
+  constructor(props) {
+        super(props);
+        this.state = {
+          right_player: true,
+          type_of_hit: []
+        };
+  }
+
+  handleClick(type){
+      var list_type_of_hits = ["service", "forehand", "backhand", "volley", "overhead"]
+      if (type === "player")
+      {
+        var this_player = this.state.right_player;
+        // console.log(this_player);
+        if (this_player)
+        {
+          this_player = false
+        }
+        else
+        {
+          this_player = true
+        }
+        this.setState({right_player:this_player})
+      }
+      if (list_type_of_hits.indexOf(type) >= 0)
+      {
+        var this_type_of_hits = this.state.type_of_hit;
+        var index = this_type_of_hits.indexOf(type);
+        if (index > -1) {
+          this_type_of_hits.splice(index, 1);
+        }
+        else {
+          this_type_of_hits.push(type);
+        }
+
+        // console.log(this_type_of_hits);
+        this.setState({type_of_hit:this_type_of_hits})
+      }
+
+
+  }
+
+  renderOptions(type){
+     /* TODO : should take t as argument for translation*/
+     return (
+       <RoundButton
+         text={type}
+         className={css`width: 100%`} value={type}
+         onClick={() => this.handleClick(type)}
+         />
+
+     );
+
+
+  }
 
   renderCourt(this_type) {
 
@@ -36,7 +95,7 @@ class HeatMap extends PureComponent {
 
       var rect = [[0,0], [2377,1097]]
 
-      console.log(this_type);
+      //console.log(hits.data);
       if (this_type === "lines"){
         return dimensions.map(el => React.createElement("line",
                                                         {x1:el[0][0] + origin_x,
@@ -63,11 +122,21 @@ class HeatMap extends PureComponent {
 
     return (
      <Container>
+        <Container>
         <svg viewBox ="0 0 2800 1500">
           {this.renderCourt("back")}
           {this.renderCourt("lines")}
         </svg>
+        </Container>
+        <ButtonContainer>
+            {this.renderOptions("player") }
+            {this.renderOptions("service") }
+            {this.renderOptions("forehand") }
+            {this.renderOptions("backhand") }
+            {this.renderOptions("volley") }
+        </ButtonContainer>
       </Container>
+
     );
   }
 }
